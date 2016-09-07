10c9198aa954ff2d38ee3250e9da34087171bfdc
Amy Eddins <amyleigheddins@gmail.com>
2016-09-07 15:29:37 -0500

    Working except for resizing height

--- public/js/app.js
+++ public/js/app.js
@@ -10,10 +10,10 @@ app.config(['$routeProvider', function($routeProvider) {
       templateUrl : 'experience.html',
       controller : 'ExperienceController'
     })
-    .when('/Portfolio', {
-      templateUrl : 'portfolio.html',
-      controller : 'PortfolioController'
-    })
+    // .when('/Portfolio', {
+    //   templateUrl : 'portfolio.html',
+    //   controller : 'PortfolioController'
+    // })
     .when('/Contact', {
       templateUrl : 'contact.html',
       controller : 'ContactController'
@@ -27,12 +27,20 @@ app.controller('MainController', ['$scope', function($scope){
   $scope.actions = [
     {"id": 1, name:"about", link: "#/About"},
     {"id": 2, name:"experience and skills", link: "#/Experience"},
-    {"id": 3, name:"portfolio", link: "#/Portfolio"},
+    // {"id": 3, name:"portfolio", link: "#/Portfolio"},
     {"id": 4, name:"github", link: "https://github.com/AmyLeighEddins"},
     {"id": 5, name:"linkedin", link: "https://www.linkedin.com/in/amy-eddins-8a223571"},
     {"id": 6, name:"contact", link: "#/Contact"},
   ];
   $scope.currentAction = $scope.actions[0];
+  $scope.toggleLinks = function() {
+    var x = document.getElementById("links");
+    if (x.className === "toggle-links") {
+        x.className += ".open";
+    } else {
+        x.className = "toggle-links";
+    }
+  };
 }]);
 
 app.controller('AboutController', ['$scope', '$location', function($scope, $location){
@@ -43,9 +51,9 @@ app.controller('ExperienceController', ['$scope', '$location', function($scope, 
 
 }]);
 
-app.controller('PortfolioController', ['$scope', '$location', function($scope, $location){
-
-}]);
+// app.controller('PortfolioController', ['$scope', '$location', function($scope, $location){
+//
+// }]);
 
 app.controller('ContactController', ['$scope', '$location', function($scope, $location){
 
