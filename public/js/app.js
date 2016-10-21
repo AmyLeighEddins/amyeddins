3ca59b2a75e112d5c6d099e16ec9bc0b206d3d03
Amy Eddins <amyleigheddins@gmail.com>
2016-10-21 17:31:20 -0500

    Moved templates, controllers, and directives into separate files and folders to fix folder/project organization. Also updated package.json.
    Moved angular and angular-route out of the js folder and included in the
    package.json file. Updated node version in package.json file.

--- public/js/app.js
+++ public/js/app.js
@@ -3,42 +3,22 @@ var app = angular.module('AmyEddins', ['ngRoute']);
 app.config(['$routeProvider', function($routeProvider) {
   $routeProvider
     .when('/About', {
-      templateUrl : 'about.html',
+      templateUrl : 'templates/about.html',
       controller : 'AboutController'
     })
     .when('/Experience', {
-      templateUrl : 'experience.html',
+      templateUrl : 'templates/experience.html',
       controller : 'ExperienceController'
     })
     // .when('/Portfolio', {
-    //   templateUrl : 'portfolio.html',
+    //   templateUrl : 'templates/portfolio.html',
     //   controller : 'PortfolioController'
     // })
     .when('/Contact', {
-      templateUrl : 'contact.html',
+      templateUrl : 'templates/contact.html',
       controller : 'ContactController'
     })
     .otherwise({
       redirectTo: '/About'
     });
-}]);
-
-app.controller('MainController', ['$scope', function($scope){
-
-}]);
-
-app.controller('AboutController', ['$scope', '$location', function($scope, $location){
-
-}]);
-
-app.controller('ExperienceController', ['$scope', '$location', function($scope, $location){
-
-}]);
-
-// app.controller('PortfolioController', ['$scope', '$location', function($scope, $location){
-//
-// }]);
-
-app.controller('ContactController', ['$scope', '$location', function($scope, $location){
-
 }]);
---
+++ public/js/controllers/experienceController.js
@@ -0,0 +1,3 @@
+app.controller('ExperienceController', ['$scope', '$location', function($scope, $location){
+
+}]);
